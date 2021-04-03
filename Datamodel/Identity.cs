018795c4cd5c7daf14b3e0e4f20a7c7dba3376e9
Lê Duy Quang <ldq@netcompany.com>
2021-04-03 15:17:28 +0700

    Fix semicolon issue

--- Datamodel/Identity.cs
+++ Datamodel/Identity.cs
@@ -4,13 +4,16 @@ namespace Planthor_ClientBackEndWebApp.Datamodel
 {
     public class Identity
     {
-        public Guid IndentiyId { get; set; };
+        public Guid IndentiyId { get; set; }
         public string IdentityProvider { get; set; }
         public string IndentityUsername { get; set; }
         public string IdentityHashPassword { get; set; }
         public Guid IdentityFacebookToken { get; set; }
 
         public Guid AccountId { get; set; }
-        public Account Account { get; set; }
+        public Account Account { get; set; 
+        
+        
+        }
     }
 }
