cd306ec0917c2ab2d5f0df72b2cd6de4dd604902
ryuugamineraito <ryuugamineraito@gmail.com>
2021-09-30 08:07:31 +0000

    clean code to make it easy to read

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-set -e
-run_cmd="dotnet ./out/PlanthorWebApiServer.dll --server.urls https://+:5000"
-
 until dotnet ef database update; do
 >&2 echo "Postgre SQL Server is starting up"
 sleep 1
 done
-# >&2 ls
->&2 echo "Postgre SQL Server is up - executing command"
-# exec $run_cmd
+
 
